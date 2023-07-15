db14b6580cf014fe407e7083699df06071e18d84
Rhys Adams <rhys@therhys.co.uk>
2023-07-15 14:49:28 +0100

    Update config, add firewall, comment code, clean up formatting.

--- configuration.nix
+++ configuration.nix
@@ -5,44 +5,36 @@
 { config, pkgs, ... }:
 
 {
-  system.stateVersion = "23.05"; # Did you read the comment?
-
   imports =
-    [ # Include the results of the hardware scan.
+    [
       "${builtins.fetchGit {
         url = "https://github.com/NixOS/nixos-hardware.git";
         rev = "1e0872458b84c2b6b3974e03ced26b87bb6df27f"; # Uses the old surface-kernel and old iptsd.
       }}/microsoft/surface/common"
+      
       "${builtins.fetchTarball {
         url = "https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz";
-      }}/nixos"
+      }}/nixos" # Home-manager
+      
       ./hardware-configuration.nix
       ./home.nix
     ];
-
-  # Allow unfree packages
+    
+  system.stateVersion = "23.05";
+  system.autoUpgrade.enable = true;
   nixpkgs.config.allowUnfree = true;
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  
+  microsoft-surface.kernelVersion = "5.19.17";
+  microsoft-surface.ipts.enable = false;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
+  networking.hostName = "thinbrick";
 
-  # Set your time zone.
   time.timeZone = "Europe/London";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_GB.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_GB.UTF-8";
     LC_IDENTIFICATION = "en_GB.UTF-8";
@@ -55,26 +47,17 @@
     LC_TIME = "en_GB.UTF-8";
   };
 
-  # Enable the X11 windowing system.
   services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
+  
   services.xserver = {
     layout = "gb";
     xkbVariant = "mac";
   };
 
-  # Configure console keymap
   console.keyMap = "uk";
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -83,36 +66,23 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    wireplumber.enable = true;
   };
 
   virtualisation = {
     podman = {
       enable = true;
-
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
       dockerCompat = true;
-
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
-      # For Nixos version > 22.11
-      #defaultNetwork.settings = {
-      #  dns_enabled = true;
-      #};
+      defaultNetwork.settings = {
+        dns_enabled = true;
+      };
     };
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  services.xserver.libinput.enable = true;
   
   programs.zsh.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.rhys = {
     isNormalUser = true;
     shell = pkgs.zsh;
@@ -125,41 +95,48 @@
     (
       self: super:
       {
-        iptsd = super.callPackage ./iptsd.nix {};
+        iptsd = super.callPackage ./iptsd.nix {}; # Enable legacy iptsd
       }
     )
   ];
 
-  services.davmail.enable = true;
+  /*services.davmail.enable = true;
   services.davmail.url = "https://outlook.office365.com/EWS/Exchange.asmx";
-  services.davmail.config = {
-    davmail.mode = "O365Manual";
-  };
+  services.davmail.config = { # Not working right now
+    davmail.mode = "EWS";
+  };*/
 
-  systemd.services.iptsd = {
+  systemd.services.iptsd = { # Enable iptsd service
     description = "IPTSD";
     path = with pkgs; [ iptsd ];
     script = "iptsd";
     wantedBy = [ "network-online.target" ];
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
     wget
     libwacom-surface
     podman
     podman-compose
+    epson-escpr
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  services.printing = {
+    enable = true;
+    drivers = [ pkgs.epson-escpr ];
+  };
+
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+  };
+
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
   fileSystems."/mnt/share" = {
     device = "server:/mnt/share";
@@ -168,32 +145,22 @@
       "nofail"
     ];
   };
+  
 
   xdg.mime.enable = true;
   xdg.mime.defaultApplications = {
-    "text" = "org.gnome.TextEditor"; # Kate is default text editor
+    "text" = "org.gnome.TextEditor"; # Gnome text editor is default text editor
   };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-
-
-  # Surface Setup
-  microsoft-surface.kernelVersion = "5.19.17";
-  microsoft-surface.ipts.enable = false;
+ 
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [
+      8080
+      3000
+    ];
+    allowedUDPPorts = [
+    
+    ];
+  };
 }
