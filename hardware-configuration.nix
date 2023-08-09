ea5306d39e29be9339040f44271447c8d5ff2786
Rhys Adams <rhys@therhys.co.uk>
2023-08-09 10:41:37 +0100

    Update config, move home to user managed home-manager.

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -24,7 +24,13 @@
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
+    swapDevices = [
+      {
+        device = "/swapfile";
+        size = 8*1024;
+      }
+    
+    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
