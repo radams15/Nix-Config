bcc063dfb34f40da4fa29b3b7821036d93b743e9
Rhys Adams <rhys@therhys.co.uk>
2024-01-27 20:38:40 +0000

    Update mktemplate package.

--- pkgs/mktemplate/default.nix
+++ pkgs/mktemplate/default.nix
@@ -1,16 +1,16 @@
 { lib, stdenv, fetchFromGitHub, buildGoModule }:
 let
-  pname = "mktemplate";
+  pname = "mktempslate";
+  version = "1.0.0";
 in
    buildGoModule {
-    inherit pname;
-    version = "1.0.0";
+    inherit pname version;
     vendorHash = null;
 
     src = fetchFromGitHub {
       owner = "radams15";
       repo = "template";
-      rev = "v1.0.0";
+      rev = "v${version}";
       sha256 = "sha256-AJazid887O/ReSFRG4O3i9s1kEbK2T/CaZLl4YbFTfE";
     };
 
