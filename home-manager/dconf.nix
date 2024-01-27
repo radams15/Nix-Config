f0a8596d18acbcc232886bf5f42a89bbf46e82c9
Rhys Adams <rhys@therhys.co.uk>
2024-01-27 20:07:09 +0000

    Add wallpaper.

--- home-manager/dconf.nix
+++ home-manager/dconf.nix
@@ -1,6 +1,12 @@
 {
   pkgs, ...
-}: {
+}: let
+  wallpaperImg = pkgs.fetchurl {
+    url = "https://images.unsplash.com/photo-1509641402296-b7f020ccf694?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=vincent-van-zalinge-TtEqhAqy_GE-unsplash.jpg&w=1920";
+    # replace this with the SHA256 hash of the image file
+    hash = "sha256-6IJINbYdNpquHOv46KCL+LYHG1PM/11wr1MBdTDc+fI=";
+  };
+in {
       dconf.settings = {
         "org/gnome/desktop/wm/preferences" = { # Left window controls
           "button-layout" = "close,minimize,maximize:appmenu";
@@ -17,6 +23,10 @@
 
         "org/gnome/mutter" = { # Scaling of X11 apps on Wayland.
           experimental-features = [ "scale-monitor-framebuffer" ];
+        };
+
+        "org/gnome/desktop/background" = {
+          picture-uri = "file://${wallpaperImg}";
         };
      };
  
