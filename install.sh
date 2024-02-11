18245df08a2e423152a35e4b73ab3ecbc8e04fd4
Rhys Adams <rhys@therhys.co.uk>
2024-02-11 15:36:02 +0000

    Install.sh now updates system.

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ if [ "$1" == "home" ]
 then
 	home-manager switch --flake .#rhys@the-brick
 else
-	sudo nixos-rebuild switch --flake .#the-brick
+	sudo nixos-rebuild switch --upgrade --flake .#the-brick
 fi
