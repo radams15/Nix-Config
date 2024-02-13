fcc3c83e487817fa1846ee5790f0099666350d00
Rhys Adams <rhys@therhys.co.uk>
2024-02-13 10:16:12 +0000

    Add update command, do update.

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@
 if [ "$1" == "home" ]
 then
 	home-manager switch --flake .#rhys@the-brick
+elif [ "$1" == "upgrade" ]
+then
+    nix flake update
+    sudo nix-channel --update
 else
 	sudo nixos-rebuild switch --upgrade --flake .#the-brick
 fi
