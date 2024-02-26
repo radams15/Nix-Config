218118cde552c5814dc5c357ff903274bba699a5
Rhys Adams <rhys@therhys.co.uk>
2024-02-26 13:12:10 +0000

    Update defaults.

--- home-manager/defaults.nix
+++ home-manager/defaults.nix
@@ -6,13 +6,13 @@
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
-      "image/jpeg" = ["org.gnome.Evince.desktop"];
+      "image/jpeg" = ["org.gnome.eog.desktop"];
+      "image/tiff" = ["org.gnome.eog.desktop"];
       "text/x-python" = ["org.gnome.TextEditor.desktop"];
       "text/markdown" = ["org.gnome.TextEditor.desktop"];
       "application/pdf" = ["org.gnome.Evince.desktop"];
       "text/x-tex" = ["org.gnome.TextEditor.desktop"];
       "text/plain" = ["org.gnome.TextEditor.desktop"];
-      "image/tiff" = ["org.gnome.eog.desktop"];
       "x-scheme-handler/discord-1170028348756471908" = ["discord-1170028348756471908.desktop"];
     };
   };
